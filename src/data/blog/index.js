e96f0374bd82f8cc8b12868943edbf8e706e1ddd
anhphong-2010 <anhphongnw.2010@gmail.com>
2022-10-07 17:36:09 +0700

    ^ generate pages

--- src/data/blog/index.js
+++ src/data/blog/index.js
@@ -3,7 +3,7 @@
 // import { ctx } from '@vl/redata';
 // import DIV from '@vl/redata/DIV.macro';
 
-// import App from '@uz/unitz-app-web/UserApp';
+// import App from '@uz/unitz-app-web/BizApp';
 // import SEO from '@uz/unitz-layout-web/SEO';
 // import { useStaticQuery, graphql } from 'gatsby';
 // import Layout from '@uz/unitz-layout-web/LayoutMain';
